032253e1ca5ac76a508a53418e680ee9e267ce14
danielgonzalezantona <danielgonzalezantona@192.168.0.203>
2016-10-01 00:08:14 +0200

    Commit inicial

---
+++ scripts/Test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export workspace=~/Documents/workspace/Daniel.Gonzalez.SVC.miw.upm.es
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_101.jdk/Contents/Home
+export M2_HOME=~/Documents/workspace/apache-maven-3.3.9
+export M2=$M2_HOME/bin
+export PATH=$M2:$PATH
+echo "-----------------------------------------"
+echo "(C) MIW"
+echo "-----------------------------------------"
+echo Workspace --- $workspace
+echo JAVA_HOME --- $JAVA_HOME
+echo M2_HOME   --- $M2_HOME
+cd $workspace
+echo "============ mvn clean test (develop por defecto) ======================================================="
+mvn clean test
